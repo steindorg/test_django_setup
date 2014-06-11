813bfffae7f5bc1e25c81201cbae9b3c81a683aa
steindor kristinsson <sgkristinsson@gmail.com>
2014-06-11 17:38:00 +0000

    Refactor home page view to use a template

--- app_1/views.py
+++ app_1/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# view fucntion
+
+# Render function is a django function that takes a request as its first paramenter and 
+# the name of the template to render as its second parameter. Django will automatically 
+# search folders called templates in my apps directories then it builds an httpresponse based on the template. 
+
 def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+	return render(request, 'home.html')
+	# return HttpResponse('<html><title>To-Do lists</title></html>')

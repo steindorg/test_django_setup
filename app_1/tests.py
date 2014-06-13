d5795baafcea0f0f51db846914aa5f93ce269d9b
steindor kristinsson <sgkristinsson@gmail.com>
2014-06-13 18:47:58 +0000

    Redirect after POST, and show all items in template

--- app_1/tests.py
+++ app_1/tests.py
@@ -32,10 +32,35 @@ from django.template.loader import render_to_string
 # which loads a template, renders it and returns the resulting string:
 
 
+from app_1.models import Item
+	# database
+
 ##########################################################################################################
 
-# Byrjum á því að gera test case klasan
 
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+
+
+# Byrjum á því að gera test case klasan
 class HomePageTest(TestCase): 
 	
 	# Maður á alltaf að láta fallið heita það sem testið á að gera.
@@ -49,6 +74,20 @@ class HomePageTest(TestCase):
 								 				# og hvernig á útkoman að vera.
  
 
+	
+	def test_home_page_only_saves_items_when_necessery(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+
+
+	
+
+
+
+
+
+
 	def test_home_page_returns_correct_html(self):
   		# We create an HttpRequest object, which is what Django 
   		# will see when a user’s browser asks for a page.
@@ -61,16 +100,24 @@ class HomePageTest(TestCase):
 
 		request.POST['item_text'] = 'A new list item'
 
-
+		
 # 		This object is an instance of a class called HttpResponse.
 		# We pass it to our home_page view, which gives us a response
 		response = home_page(request)
+		
+		self.assertEqual(Item.objects.count(),1)
+		new_item = Item.objects.first()
+		print ( new_item.text )
+		self.assertEqual(new_item.text, 'A new list item')
+
+
+
 										# HttpResponse.content
 										# A bytestring representing the content, encoded from a Unicode object if necessary.
 										# We use .decode() to convert the response.content bytes into a Python unicode string, 
 										# which allows us to compare strings with strings, 
 										# instead of bytes with bytes as we did earlier.
-		self.assertIn('A new list item', response.content.decode())
+		#self.assertIn('A new list item', response.content.decode())
 		
 						# Django provides a shortcut function which largely automates the process: 
 						# render_to_string() in django.template.loader, 
@@ -102,9 +149,48 @@ class HomePageTest(TestCase):
 		# Now We use .decode() to convert the response.content bytes into a Python 
 		# unicode string, which allows us to compare strings with strings, 
 		# instead of bytes with bytes as we did earlier.
-		self.assertEqual(response.content.decode(), expected_html) 
+		#self.assertEqual(response.content.decode(), expected_html) 
 
         
         
-		self.assertIn(b'<title>To-Do lists</title>', response.content)  
-		self.assertTrue(response.content.endswith(b'</html>'))
+		#self.assertIn(b'<title>To-Do lists</title>', response.content)  
+		#self.assertTrue(response.content.endswith(b'</html>'))
+
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+		
+	def test_home_page_redirects_after_post(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_home_page_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		request = HttpRequest()
+		response = home_page(request)
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
+
+
+
+
+
+
--- app_1/views.py
+++ app_1/views.py
@@ -1,27 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from app_1.models import Item
 
 # Render function is a django function that takes a request as its first paramenter and 
 # the name of the template to render as its second parameter. Django will automatically 
 # search folders called templates in my apps directories then it builds an httpresponse based on the template. 
 
 def home_page(request):
-	#if request.method == 'post':
-	#	return HttpResponse(request.post['item_text'])
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'])
+
+		return redirect('/')
+
 	
-
+	# notað til að skila lista objectum og til að gera lúppu í html sjá home.html
+	items = Item.objects.all()
 			# Combines a given template with a given context dictionary and returns an HttpResponse object with that rendered text.
-	return render(request, 'home.html', {
-		#'new_item_text': request.POST['item_text'],
-		# ný útfærsla 
-								# hvad er ad gerast herna 
-								# get(key[, default])¶
-								# Return the value for key if key is in the dictionary, else default. 
-								# If default is not given, it defaults to None, so that this method never raises 
-								# a KeyError.
-								# QueryDict.get(key, default)
-
-								# 
-		'new_item_text': request.POST.get('item_text',''),	
-	})
+	return render(request, 'home.html', {'items': items})
 	# return HttpResponse('<html><title>To-Do lists</title></html>')

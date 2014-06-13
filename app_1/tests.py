10a4fe20b990380352051148006236a5c91afd8c
steindor kristinsson <sgkristinsson@gmail.com>
2014-06-13 12:44:05 +0000

    "before further tests on the table input"

--- app_1/tests.py
+++ app_1/tests.py
@@ -25,6 +25,13 @@ from django.http import HttpRequest
 from app_1.views import home_page
 
 
+from django.template.loader import render_to_string
+
+# To cut down on the repetitive nature of loading and rendering templates, 
+# Django provides a shortcut function which largely automates the process: render_to_string() in django.template.loader, 
+# which loads a template, renders it and returns the resulting string:
+
+
 ##########################################################################################################
 
 # Byrjum á því að gera test case klasan
@@ -47,13 +54,56 @@ class HomePageTest(TestCase):
   		# will see when a user’s browser asks for a page.
 		request = HttpRequest() 
 		
-		# This object is an instance of a class called HttpResponse.
+		# method : String containing the HTTP method used by the request ('GET' or 'POST')
+		request.method = 'POST'
+		
+		# POST: A QueryDict representing form values submitted using the HTTP POST method
+
+		request.POST['item_text'] = 'A new list item'
+
+
+# 		This object is an instance of a class called HttpResponse.
 		# We pass it to our home_page view, which gives us a response
 		response = home_page(request)
+										# HttpResponse.content
+										# A bytestring representing the content, encoded from a Unicode object if necessary.
+										# We use .decode() to convert the response.content bytes into a Python unicode string, 
+										# which allows us to compare strings with strings, 
+										# instead of bytes with bytes as we did earlier.
+		self.assertIn('A new list item', response.content.decode())
+		
+						# Django provides a shortcut function which largely automates the process: 
+						# render_to_string() in django.template.loader, 
+						# which loads a template, renders it and returns the resulting string: 
+						# The function takes one argument " The template name " which is the name of the template
+						# that should be loaded and rendered and two optional arguments 
+						# ---Second argument--- is a dictionary. A dictionary to be used as variables 
+						# and values for the template’s context. This can also be passed as the second positional argument.
+						# ---Third--- is Context_instance. An instance of Context or a subclass (e.g., an instance of RequestContext) 
+						# to use as the template’s context. This can also be passed as the third positional argument.
+		expected_html = render_to_string(
+			'home.html',
+			# New item text er python variable sem er declerað í töflu í htmlinu
+			# The render_to_string function takes, as its second parameter, a mapping of variable names to values. 
+			# We’re giving the template a variable named new_item_text, whose value is the expected item text from our POST request.
+			{'new_item_text': 'A new list item'}
+		)
+		
+
+		
+
+
+		# expected_html = render_to_string('home.html')
 		
 		# we have to use the b'' syntax to compare because response.content is rawbytes and not a
 		# python string which means that b '' converts whats there into a raw byte file 
-		self.assertTrue(response.content.startswith(b'<html>'))  
+		# self.assertTrue(response.content.startswith(b'<html>'))  
+
+		# Now We use .decode() to convert the response.content bytes into a Python 
+		# unicode string, which allows us to compare strings with strings, 
+		# instead of bytes with bytes as we did earlier.
+		self.assertEqual(response.content.decode(), expected_html) 
+
         
         
 		self.assertIn(b'<title>To-Do lists</title>', response.content)  
--- app_1/views.py
+++ app_1/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 # Render function is a django function that takes a request as its first paramenter and 
 # the name of the template to render as its second parameter. Django will automatically 
 # search folders called templates in my apps directories then it builds an httpresponse based on the template. 
 
 def home_page(request):
-	return render(request, 'home.html')
+	#if request.method == 'post':
+	#	return HttpResponse(request.post['item_text'])
+	
+
+			# Combines a given template with a given context dictionary and returns an HttpResponse object with that rendered text.
+	return render(request, 'home.html', {
+		#'new_item_text': request.POST['item_text'],
+		# ný útfærsla 
+								# hvad er ad gerast herna 
+								# get(key[, default])¶
+								# Return the value for key if key is in the dictionary, else default. 
+								# If default is not given, it defaults to None, so that this method never raises 
+								# a KeyError.
+								# QueryDict.get(key, default)
+
+								# 
+		'new_item_text': request.POST.get('item_text',''),	
+	})
 	# return HttpResponse('<html><title>To-Do lists</title></html>')

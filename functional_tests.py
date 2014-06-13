45db96d588d1759fcf2c18d5c28d6b6430a44254
steindor kristinsson <sgkristinsson@gmail.com>
2014-06-13 14:04:34 +0000

    "functional test changes"

--- functional_tests.py
+++ functional_tests.py
@@ -43,6 +43,11 @@ class NewVisitorTest(unittest.TestCase):  #1
 	def tearDown(self):  
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 
 	def test_can_start_a_list_and_retrieve_it_later(self):  #4
         # Edith has heard about a cool new online to-do app. She goes
@@ -51,7 +56,7 @@ class NewVisitorTest(unittest.TestCase):  #1
 
 
 		# she notices the page title and header mention to-do-list
-		self.assertIn('To-Do', self.browser.title)  #5
+		self.assertIn('To-Do', self.browser.title)  
 		
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		
@@ -72,10 +77,7 @@ class NewVisitorTest(unittest.TestCase):  #1
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		
 		
@@ -91,13 +93,9 @@ class NewVisitorTest(unittest.TestCase):  #1
 
 
 		# The page updates again, and now shows both items on her list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn(
-			'2: Use peacock feathers to make a fly' ,
-			[row.text for row in rows]
-		)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect.

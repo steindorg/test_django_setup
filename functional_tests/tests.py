d2e25d7c7a13203e02ae059b1f03d969f7fd9a77
steindor kristinsson <sgkristinsson@gmail.com>
2014-06-17 19:59:17 +0000

    new url + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -117,7 +117,7 @@ class NewVisitorTest(LiveServerTestCase):  #1
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('buy milk')
+		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
 
 

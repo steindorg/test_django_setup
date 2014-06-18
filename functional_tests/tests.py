838c9c7e25ffb0ff5386c17f6e4207020e44e8dd
steindor kristinsson <sgkristinsson@gmail.com>
2014-06-18 17:21:07 +0000

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -50,6 +50,30 @@ class NewVisitorTest(LiveServerTestCase):  #1
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
 
+	def test_layout_and_styling(self):
+		# Edith goes to the homepage
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# she notices the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+
+
+			)
+	
+  		# She starts a new list and sees the input is nicely
+    	# centered there too
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):  #4
         # Edith has heard about a cool new online to-do app. She goes
